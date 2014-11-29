6745664bc9d67eac502bd30c5515ed758f0c3857
FireNero <firenero94@gmail.com>
2014-11-29 21:32:50 +0200

    - Fix correct input checking for Ford-Bellman's algorithm.

--- GraphEditor/GraphEditor/Algorithms/FordBellmanAlgorithm.cs
+++ GraphEditor/GraphEditor/Algorithms/FordBellmanAlgorithm.cs
@@ -34,15 +34,8 @@ namespace GraphEditor.Algorithms
 				message = "Only 2 vertices must be selected.";
 				return false;
 			}
-			if (GetSelectedVertices().Count != 2)
-			{
-				message = "Only 2 vertices must be selected.";
-				return false;
-			}
 			message = "There is no path between selected vertices.";
-			var isUsed = SameTreeChecking();
-			return isUsed[(GraphElementVertex) HelperFunctions.GetGraphElement(DrawingCanvas, GetSelectedVertices()[0])] &&
-			       isUsed[(GraphElementVertex) HelperFunctions.GetGraphElement(DrawingCanvas, GetSelectedVertices()[1])];
+			return true;
 		}
 
 		protected override AlgorithmResult RunAlgorithm()
@@ -112,45 +105,6 @@ namespace GraphEditor.Algorithms
 			var result = new List<GraphElementBase>(resultVertexes);
 			result.AddRange(resultEdges);
 			return new AlgorithmResult(dist[endVertex], result);
-		}
-
-		private bool HasNegativeCycle()
-		{
-			var vertices = DrawingCanvas.GraphStructure.Vertices;
-			var edges = DrawingCanvas.GraphStructure.GetAllEdges().ToList();
-			var startVertex = (GraphElementVertex)HelperFunctions.GetGraphElement(DrawingCanvas, GetSelectedVertices().First());
-
-			var dest = new Dictionary<GraphElementVertex, double>(vertices.Count);
-			var path = new Dictionary<GraphElementVertex, GraphElementVertex>();
-
-			var resultVertexes = new List<GraphElementVertex>();
-			var resultEdges = new HashSet<GraphElementEdge>();
-
-			for (int i = 0; i < vertices.Count; i++)
-			{
-				dest.Add(vertices[i], Double.PositiveInfinity);
-				path.Add(vertices[i], null);
-			}
-			dest[startVertex] = 0.0;
-
-			GraphElementVertex x = null;
-			for (int i = 0; i < vertices.Count; i++)
-			{
-				x = null;
-				for (int j = 0; j < edges.Count; j++)
-				{
-					if (!Double.IsPositiveInfinity(dest[edges[j].Begin]))
-					{
-						if (dest[edges[j].End] > dest[edges[j].Begin] + edges[j].Weight)
-						{
-							dest[edges[j].End] = Math.Max(Double.NegativeInfinity, dest[edges[j].Begin] + edges[j].Weight);
-							path[edges[j].End] = edges[j].Begin;
-							x = edges[j].End;
-						}
-					}
-				}
-			}
-			return x == null;
 		}
 	}
 }
